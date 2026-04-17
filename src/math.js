19cdde1cd0ee1d157082b6fe39a57a335e1a87b6
fatimazahrabaali <f.baali3715@uca.ac.ma>
2026-04-17 22:26:29 +0100

    fix: export subtract function

--- src/math.js
+++ src/math.js
@@ -1,18 +1,20 @@
 // src/math.js
 function add(a, b) {
-return a + b;
+  return a + b;
 }
+
 function multiply(a, b) {
-return a * b;
+  return a * b;
 }
+
 function factorial(n) {
-if (n < 0) throw new Error('Nombre négatif');
-if (n <= 1) return 1;
-return n * factorial(n - 1);
+  if (n < 0) throw new Error('Nombre négatif');
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
 }
+
 function subtract(a, b) {
   return a - b;
 }
-// N'oublie pas de l'ajouter dans module.exports :
+
 module.exports = { add, multiply, factorial, subtract };
-module.exports = { add, multiply, factorial };
